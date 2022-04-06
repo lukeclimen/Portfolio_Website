d18e00ed63d008a3014dfb564ec3d89944e8e3ec
Luke Climenhage <luke.climenhage@gmail.com>
2022-04-06 11:26:31 -0400

    Updated jQuery and created darkmode function

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,19 @@
 /* Navigation bar expand/contract */
 
-const collapsible = document.querySelectorAll(".collapsible");
-collapsible.forEach((item) =>
-    item.addEventListener("click", function () {
-        this.classList.toggle("collapsible--expanded");
-    })
-);
+ $(".nav__brick").click(function() {
+    $(".collapsible").get(0).classList.toggle("collapsible--expanded");
+ });
+
+/* Dark Mode / Light mode Toggler */
+
+$(".lightmode__toggle").click(function (){
+    document.body.classList.toggle("darkmode");
+    let image = $(this).attr("src");
+    if (image.includes("sun")) {
+        $(this).attr("src", image.replace("sun", "moon"));
+    }
+    else {       
+        $(this).attr("src", image.replace("moon", "sun"));
+    }
+});
+
