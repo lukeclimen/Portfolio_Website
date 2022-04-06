49bd2d7aad99ff1fefb65c164d9294b71eb77d58
Luke Climenhage <luke.climenhage@gmail.com>
2022-04-06 11:41:56 -0400

    Added components and colours to dark mode

--- javascript/main.js
+++ javascript/main.js
@@ -9,11 +9,14 @@
 $(".lightmode__toggle").click(function (){
     document.body.classList.toggle("darkmode");
     let image = $(this).attr("src");
+    let logo = $(".nav__logo").attr("src");
     if (image.includes("sun")) {
         $(this).attr("src", image.replace("sun", "moon"));
+        $(".nav__logo").attr("src", logo.replace("light", "dark"));
     }
     else {       
         $(this).attr("src", image.replace("moon", "sun"));
+        $(".nav__logo").attr("src", logo.replace("dark", "light"));
     }
 });
 
