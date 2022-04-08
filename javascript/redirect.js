1b1d1b11bab2a68d7b5a772a596d235a61f0cb59
Luke Climenhage <luke.climenhage@gmail.com>
2022-04-08 18:13:13 -0400

    Updated js files to reflect php changes

--- javascript/redirect.js
+++ javascript/redirect.js
@@ -1,4 +1,4 @@
 // Redirects to the homepage after 15seconds
 setTimeout(function(){
-            window.location.href = 'index.html';
+            window.location.href = 'index.php';
          }, 5000);
