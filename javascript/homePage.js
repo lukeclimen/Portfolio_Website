9cc214bfeafef5999914a7750461d06cdea82235
Luke Climenhage <luke.climenhage@gmail.com>
2022-04-08 13:40:50 -0400

    Created animation slideshow for homepage logos

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -1,5 +1,5 @@
-// Chooses the home page image(s) based on screen width
 $(function() {
+    // Chooses the home page image(s) based on screen width
    let size = window.innerWidth;
     console.log(size);
     if (size >= 852) {
@@ -8,7 +8,15 @@ $(function() {
     else {
         $("#self__image").attr("src", "images/Mobile_Me.JPG");
     }
-    
+
+    let i = 0;
+    //Creates a "slideshow" of logo's in the Past Jobs section
+    setInterval(function() {
+        slideshow(i);
+        i++;
+    }, 3 * 1000);
+
+    //Event listener for window size changing
     $(window).resize(function() {
         let newSize = window.innerWidth;
         console.log(newSize);
@@ -21,3 +29,10 @@ $(function() {
     });
 });
 
+function slideshow(i) {
+    let logos = ["images/grf.png", "images/PepsiCo.svg", "images/CESi.png"];
+    let transition = "images/white_circle.svg";
+    let logoImage = $(".logo__image");
+    let element = (i % 3);
+    logoImage.attr("src", logos[element]);
+}
